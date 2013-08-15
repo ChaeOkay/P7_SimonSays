8649a31bc0ca1b0136c7602081577d6a7db0376a
cokeefe <chaemobile@gmail.com>
2013-08-15 09:08:13 -0700

    ajax working

--- public/js/application.js
+++ public/js/application.js
@@ -2,16 +2,11 @@ $(document).ready(function(){
 
   $('#get_color').on('click', function(){
 
-    // var current_colors = $('ul li').css('color');
-    // console.log(current_colors);
-
     $.ajax({
       type: 'POST',
       url: '/color',
     }).done(function(response){
-      console.log(response); //why isn't this json? post request?
-        console.log($("#color_me"));
-      // change html/css with jquery
+     $("ul li").eq(response.cell).css("background-color", response.color);
     });
   });
 
