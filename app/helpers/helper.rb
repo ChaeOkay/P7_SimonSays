edb0b345b76b88d683be6af439cd9af77c2cba6d
cokeefe <chaemobile@gmail.com>
2013-08-15 09:06:27 -0700

    ajax response okay

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -1,2 +1,11 @@
 helpers do
+
+  def random_button
+    cell= rand(1..9)
+    color= "#" + "%06x" % (rand * 0xffffff)
+
+    content_type :json
+    { cell: cell, color: color }.to_json
+  end
+
 end
