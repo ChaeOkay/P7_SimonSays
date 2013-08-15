86ecd015a52c8cfca3bdefc11dc0780730f6fc29
cokeefe <chaemobile@gmail.com>
2013-08-15 11:00:09 -0700

    success message

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-
   $('#get_color').on('click', function(){
 
     $.ajax({
       type: 'POST',
       url: '/color',
+
     }).done(function(response){
      $("ul li").eq(response.cell).css("background-color", response.color);
-    });
-  });
 
+    }).success(function(data, textStatus, jqXHR){
+      console.log(textStatus + " " + data.cell + " " + data.color);
+    });
+
+  });
 });
